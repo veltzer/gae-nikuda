e4d9e552c20183902a1c20ce099637eb78c5b7ba
Mark Veltzer <mark.veltzer@gmail.com>
2013-11-01 19:10:32 +0200

    another script fixed

--- php/nikuda_ed.php
+++ php/nikuda_ed.php
@@ -1,22 +1,21 @@
 ﻿<?php
-	// connect to host DataBase
-	mysql_connect("localhost","reemj_doron","doron15")
-		or die("Could not connect: " . mysql_error());
-	mysql_select_db("reemj_nikuda")
-		or die("Could not select DB: " . mysql_error());
+	require_once 'utils.php';
 
-	mysql_set_charset("hebrew")
-		or die("Could not set Hebrew character set: " . mysql_error());
+	// start the page
+	nikuda_initpage();
+	// connect to host DataBase
+	nikuda_connect();
 
 	$Request = $_POST['Request']
 	// validate client
 	$ValidateClient = "SELECT Name FROM clients WHERE Name = '$Request->Name' AND Code = '$Request->Code'";
 
-	$Valid = mysql_query($ValidateClient)
-		or die("Failed in validating client:" . mysql_error());
+	global $link;
+	$Valid = mysqli_query($link, $ValidateClient)
+		or die("Failed in validating client:" . mysqli_error());
 
 	// if succesfull than add
-	if (mysql_fetch_array($Valid)) {
+	if (mysqli_fetch_array($Valid)) {
 
 		// get inserted words
 		// decode JSON string to php object
@@ -25,12 +24,14 @@
 		foreach ($RequestedWords as $Word){
 			$AddWord = "INSERT IGNORE INTO wordlist VALUES(NULL, '$Word->Naked', '$Word->Nikud')";
 
-			mysql_query($AddWord)
-				or die("Failed in adding word:" . '$Word->Naked . ", error:" . mysql_error());
+			mysqli_query($link, $AddWord)
+				or die("Failed in adding word:" . $Word->Naked . ", error:" . mysqli_error());
 			}
 			else {
 				die("Client validation failed.");
 			}
 		}
 	}
+	nikuda_disconnect();
+	nikuda_finipage();
 ?>
