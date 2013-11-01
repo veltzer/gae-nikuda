8784532a28827d1ed6d4591db9b6d673fddbd1d2
Mark Veltzer <mark.veltzer@gmail.com>
2013-11-01 19:08:14 +0200

    fixed up another script

--- php/nikuda_suggest.php
+++ php/nikuda_suggest.php
@@ -1,18 +1,17 @@
 ﻿<?php
-	// connect to host DataBase
-	mysql_connect("localhost","reemj_doron","doron15")
-		or die("Could not connect: " . mysql_error());
-	mysql_select_db("reemj_nikuda")
-		or die("Could not select DB: " . mysql_error());
+	require_once 'utils.php';
 
-	mysql_set_charset("hebrew")
-		or die("Could not set Hebrew character set: " . mysql_error());
+	// start the page
+	nikuda_initpage();
+	// connect to host DataBase
+	nikuda_connect();
 
 	// get queried word
 	// decode JSON string to php object
 	$QueriedWord = $_POST['Word'];
 
 	$Naked = $QueriedWord['Naked'];
+	global $link;
 
 	if (strlen($Naked) >= 3) {
 		$Reply = array();
@@ -21,15 +20,17 @@
 
 		// Get word's possible completions from DB
 		$GetSuggestions = "SELECT DISTINCT Naked FROM wordlist WHERE Naked LIKE '$Naked%' LIMIT 50"; // add ORDER BY Frequency when this is implemented in DB
-		$Nakeds = mysql_query($GetSuggestions)
-			or die("Failed in selecting similar words:" . mysql_error());
+		$Nakeds = mysqli_query($link, $GetSuggestions)
+			or die("Failed in selecting similar words:" . mysqli_error($link));
 
 		// Push the word's punctuations into the word's reply
-		while ($Naked = mysql_fetch_array($Nakeds)) {
+		while ($Naked = mysqli_fetch_array($Nakeds)) {
 			array_push($Reply['Nakeds'], $Naked[0]);
 		}
 
 		// Echo encoded reply
 		echo json_encode($Reply);
 	}
+	nikuda_disconnect();
+	nikuda_finipage();
 ?>
