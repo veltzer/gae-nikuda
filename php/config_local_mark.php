94698548004bdfeb7ec284780e8a8476cc105781
Mark Veltzer <mark.veltzer@gmail.com>
2013-11-15 23:19:56 +0200

    marks config

---
+++ php/config_local_mark.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Here is the php site configuration for nikuda
+ */
+$db_host='localhost';
+$db_user='mark';
+$db_pass='';
+$db_name='nikuda';
+$db_charset='utf8';
+$do_log_errors=true;
+$do_ob=true;
+$do_utf_headers=true;
+?>
