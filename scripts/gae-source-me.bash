d58f7e3c547865d14ecbb5eb5280c31730d6b7cc
Mark Veltzer <mark.veltzer@gmail.com>
2015-09-14 05:26:36 +0300

    added gae (instead of being always configured)

---
+++ scripts/gae-source-me.bash
@@ -0,0 +1,5 @@
+# google cloud stuff
+# The next line updates PATH for the Google Cloud SDK.
+source ~/install/google-cloud-sdk/path.bash.inc
+# The next line enables bash completion for gcloud.
+source ~/install/google-cloud-sdk/completion.bash.inc
