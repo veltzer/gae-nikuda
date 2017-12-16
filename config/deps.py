3848fab58d60f63fae84c42e9909832a1579e7fc
Mark Veltzer <mark.veltzer@gmail.com>
2017-12-16 23:41:54 +0200

    better config for pydmt

--- config/deps.py
+++ config/deps.py
@@ -1,52 +1,40 @@
-"""
-dependencies for this project
-"""
-
-
-def populate(d):
-    d.tools = [
-        'jsl',
-        'jsmin',
-        'closure',
-        'css-validator',
-    ]
-    d.packs = [
-        # for validating html
-        'tidy',
-        # for uploading via ftp
-        'ncftp',
-        # for jsl and other stuff
-        'python',
-        # for mysql
-        'mysql-server',
-        # for local apache web server
-        'apache2',
-        # for php support for apache
-        'libapache2-mod-php',
-        # for mysql support for php
-        'php-mysql',
-        # for json support for php
-        'php-json',
-        # for the css-validator (which is written in java) to work
-        'oracle-java7-installer',
-        # for templating
-        'gpp',
-        # for the local gcloud server to be able to do php
-        'php-cgi',
-        # other ftp client
-        'lftp',
-        # for jslint
-        'nodejs',
-        # for jslint
-        'npm',
-        # for gjslint
-        'closure-linter',
-        # for csstidy
-        'csstidy',
-    ]
-
-
-def get_deps():
-    return [
-        __file__,  # myself
-    ]
+tools = [
+    'jsl',
+    'jsmin',
+    'closure',
+    'css-validator',
+]
+packs = [
+    # for validating html
+    'tidy',
+    # for uploading via ftp
+    'ncftp',
+    # for jsl and other stuff
+    'python',
+    # for mysql
+    'mysql-server',
+    # for local apache web server
+    'apache2',
+    # for php support for apache
+    'libapache2-mod-php',
+    # for mysql support for php
+    'php-mysql',
+    # for json support for php
+    'php-json',
+    # for the css-validator (which is written in java) to work
+    'oracle-java7-installer',
+    # for templating
+    'gpp',
+    # for the local gcloud server to be able to do php
+    'php-cgi',
+    # other ftp client
+    'lftp',
+    # for jslint
+    'nodejs',
+    # for jslint
+    'npm',
+    # for gjslint
+    'closure-linter',
+    # for csstidy
+    'csstidy',
+]
