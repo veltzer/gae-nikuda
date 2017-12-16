3ff6b58cbc0824ccbf66da70191968bdda245d4c
Mark Veltzer <mark.veltzer@gmail.com>
2017-12-16 17:05:01 +0200

    new dbs

--- datastore/query_all_to_local.py
+++ datastore/query_all_to_local.py
@@ -12,12 +12,12 @@ kind = 'Diacritics'
 client = datastore.Client()
 # noinspection PyTypeChecker
 query = client.query(kind=kind)
-# results = list(query.fetch(10))
-results = list(query.fetch())
+results = list(query.fetch(10))
+# results = list(query.fetch())
 d = {}
 for result in results:
     d[result['raw']] = result['possible_diacritics']
-with codecs.open("db/all.json", "wt", encoding="utf-8") as fp:
+with codecs.open("db/10.json", "wt", encoding="utf-8") as fp:
     json.dump(d, fp)
-with codecs.open("db/all.pickle", "w") as fp:
-    pickle.dump(d, fp)
+with open("db/10.pickle", "wb") as fp:
+    pickle.dump(d, fp, pickle.HIGHEST_PROTOCOL)
