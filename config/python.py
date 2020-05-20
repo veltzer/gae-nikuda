42e483463949372018a9ea5fadc60e5300e56777
Mark Veltzer <mark.veltzer@gmail.com>
2020-05-20 17:23:26 +0300

    templated reqs

---
+++ config/python.py
@@ -0,0 +1,25 @@
+import config.project
+
+package_name = config.project.project_name
+
+console_scripts = [
+]
+
+setup_requires = [
+]
+
+run_requires = [
+    'flask',  # for a web framework
+]
+
+test_requires = [
+]
+
+dev_requires = [
+    'pydmt',  # for templating
+]
+
+install_requires = list(setup_requires)
+install_requires.extend(run_requires)
+
+python_requires = ">=3.5"
