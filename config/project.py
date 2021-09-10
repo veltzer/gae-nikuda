72804ec5d4a75aa50608cd0a2f3661bdbdd93bcf
Mark Veltzer <mark.veltzer@gmail.com>
2021-09-10 19:24:53 +0300

    fix config

--- config/project.py
+++ config/project.py
@@ -1,16 +1,15 @@
 import datetime
 import config.general
 
-project_name = 'nIkUdAh'
-project_long_description = 'Nikuda web site'
-project_year_started = '2004'
-project_description = '''Nikuda is a web site intended to allow
-users to get punctuation for hebrew words, sentences and paragraphs.'''
+project_name = "nIkUdAh"
+project_long_description = "Nikuda web site"
+project_year_started = "2004"
+project_description = """Nikuda is a web site intended to allow
+users to get punctuation for hebrew words, sentences and paragraphs."""
 
-project_copyright_years = ', '.join(
+project_copyright_years = ", ".join(
     map(str, range(int(project_year_started), datetime.datetime.now().year + 1)))
 if str(config.general.general_current_year) == project_year_started:
     project_copyright_years = config.general.general_current_year
 else:
-    project_copyright_years = '{0}-{1}'.format(project_year_started, config.general.general_current_year)
-
+    project_copyright_years = "{0}-{1}".format(project_year_started, config.general.general_current_year)
