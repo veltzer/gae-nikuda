fb3137276953c5185209285ec37967926ae79d9e
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-27 00:33:02 +0300

    better project config

--- config/project.py
+++ config/project.py
@@ -3,7 +3,7 @@ import config.general
 
 project_name = "nIkUdAh"
 project_long_description = "Nikuda web site"
-project_year_started = "2004"
+project_year_started = 2004
 project_description = """Nikuda is a web site intended to allow
 users to get punctuation for hebrew words, sentences and paragraphs."""
 
