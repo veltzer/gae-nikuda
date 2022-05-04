603dbcc3df19a914e249da14113f71cc77607d70
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-04 03:22:21 +0300

    now working

--- config/deps.py
+++ config/deps.py
@@ -1,34 +1,10 @@
 packages = [
     # for validating html
     'tidy',
-    # for uploading via ftp
-    'ncftp',
-    # for jsl and other stuff
-    'python',
-    # for mysql
-    'mysql-server',
-    # for local apache web server
-    'apache2',
-    # for php support for apache
-    'libapache2-mod-php',
-    # for mysql support for php
-    'php-mysql',
-    # for json support for php
-    'php-json',
-    # for the css-validator (which is written in java) to work
-    'oracle-java7-installer',
-    # for templating
-    'gpp',
-    # for the local gcloud server to be able to do php
-    'php-cgi',
-    # other ftp client
-    'lftp',
     # for jslint
     'nodejs',
     # for jslint
     'npm',
-    # for gjslint
-    'closure-linter',
     # for csstidy
     'csstidy',
 ]
