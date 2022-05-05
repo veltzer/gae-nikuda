bbf3bef3f9f03c4f7eac27dade9ef3daa02eda24
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 01:07:20 +0300

    fixing build

--- config/python.py
+++ config/python.py
@@ -2,25 +2,18 @@ import config.project
 
 package_name = config.project.project_name
 
-console_scripts = [
-]
-
-setup_requires = [
-]
-
+console_scripts = []
+setup_requires = []
 run_requires = [
     'flask',  # for a web framework
 ]
-
-test_requires = [
-]
-
+test_requires = []
 dev_requires = [
-    'pydmt',  # for templating
-    'pymakehelper',  # for the makefile
+    'pydmt',
+    'pymakehelper',
 ]
 
 install_requires = list(setup_requires)
 install_requires.extend(run_requires)
 
-python_requires = ">=3.5"
+python_requires = ">=3.9"
