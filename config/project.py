97c31831313f18d79610bab72d9db2319e695bff
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:51:02 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,15 +1,4 @@
-import datetime
-import config.general
-
-project_name = "nIkUdAh"
-project_long_description = "Nikuda web site"
-project_year_started = 2004
-project_description = """Nikuda is a web site intended to allow
+name_fancy = "nIkUdAh"
+description_short = "Nikuda web site"
+description_long = """Nikuda is a web site intended to allow
 users to get punctuation for hebrew words, sentences and paragraphs."""
-
-project_copyright_years = ", ".join(
-    map(str, range(int(project_year_started), datetime.datetime.now().year + 1)))
-if str(config.general.general_current_year) == project_year_started:
-    project_copyright_years = config.general.general_current_year
-else:
-    project_copyright_years = f"{project_year_started} - {config.general.general_current_year}"
