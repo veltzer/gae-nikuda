fa73fb91e4f1c93f95f77684c8c476484df91223
Mark Veltzer <mark.veltzer@gmail.com>
2023-11-19 00:26:54 +0200

    keywords adjust

--- config/project.py
+++ config/project.py
@@ -3,3 +3,4 @@ name_fancy = "nIkUdAh"
 description_short = "Nikuda web site"
 description_long = """Nikuda is a web site intended to allow
 users to get punctuation for hebrew words, sentences and paragraphs."""
+keywords = []
