0cf1ec474ece3983c9abb38431b11f2178addcc8
Mark Veltzer <mark.veltzer@gmail.com>
2024-01-12 15:06:34 +0200

    more python.py changes

--- config/python.py
+++ config/python.py
@@ -3,9 +3,9 @@ dev_requires = []
 install_requires = [
     "flask",
 ]
-make_requires = [
+build_requires = [
     "pymakehelper",
     "pydmt",
 ]
 test_requires = []
-requires = config_requires + install_requires + make_requires + test_requires
+requires = config_requires + install_requires + build_requires + test_requires
