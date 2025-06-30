07b975753832c88aaceae9e7f45d8fa1fdbbed72
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-30 13:47:53 +0300

    fixed up deps

--- config/python.py
+++ config/python.py
@@ -1,12 +1,11 @@
 """ python deps for this project """
 
+import config.shared
+
 install_requires: list[str] = [
     "flask",
     "gunicorn",
 ]
-build_requires: list[str] = [
-    "pydmt",
-    "pymakehelper",
-    "pylint",
-]
-requires = install_requires + build_requires
+build_requires: list[str] = config.shared.BUILD
+test_requires: list[str] = config.shared.TEST
+requires = install_requires + build_requires + test_requires
